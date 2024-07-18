e4396c65d38e7984f32527c2da468952e3585368
CarlosHenriqueMkt <falacom.ocarlos@gmail.com>
2024-07-18 07:54:03 -0300

    Interactive Mapbox City

---
+++ src/data/mapbox.js
@@ -0,0 +1,30 @@
+// data.js
+export const pointsData = [
+	{
+		coordinates: [55.274376, 25.197197],
+		description: "Burj Khalifa",
+		alarms: 5,
+		workOrders: 10,
+		activeWO: 3,
+		closedWO: 7,
+		slaMet: "70%",
+	},
+	{
+		coordinates: [55.2774, 25.1972],
+		description: "Dubai Mall",
+		alarms: 2,
+		workOrders: 8,
+		activeWO: 2,
+		closedWO: 6,
+		slaMet: "70%",
+	},
+	{
+		coordinates: [55.2719, 25.1955],
+		description: "Dubai Opera",
+		alarms: 1,
+		workOrders: 5,
+		activeWO: 1,
+		closedWO: 4,
+		slaMet: "70%",
+	},
+];
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/Login";
 import VXBoard from "../pages/Dashboard";
 import NotFound from "../pages/NotFound";
+import Dubai from "../pages/DubaiCity";
 
 const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<LoginPage />} />
 			<Route path="/dashboard/*" element={<VXBoard />} />
+			<Route path="/dubai" element={<Dubai />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
